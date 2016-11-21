7fe539e7bdc7691d1572ec4ad9ff752c5f9f8c19
Dmitry Konovalov <dmitry.konovalov@masterdata.ru>
2016-11-21 16:02:08 +0300

    dd

--- src/main/java/ru/spec/java/HelloWorld.java
+++ src/main/java/ru/spec/java/HelloWorld.java
@@ -1,9 +1,11 @@
 package ru.spec.java;
 
 public class HelloWorld {
+	
+	private static final String HELLO_MESSAGE = "Hello World!";
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println(HELLO_MESSAGE);
 	}
 
 }
